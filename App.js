04e60dc705f956e064652b83be24feb4bf8ca97b
afscomercial <afscomercial@gmail.com>
2021-08-30 12:41:20 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {withAuthenticator} from 'aws-amplify-react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
+
+export default withAuthenticator(App);
